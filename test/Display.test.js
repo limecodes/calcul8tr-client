579a4a15eb54611ae0a2e8ceb477d9a4859945c8
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-08-03 10:02:22 +0300

    Changed jest config and modified test.

--- test/Display.test.js
+++ test/Display.test.js
@@ -5,7 +5,7 @@ import Display from '../src/components/Display';
 describe('Basic Display Functionality', () => {
   test('Should display given value', () => {
     const display = renderer.create(<Display value={'0'} />);
-    const instance = renderer.root;
+    const instance = display.root;
 
     expect(instance.findByType(Display).props.value).toBe('0');
   });
